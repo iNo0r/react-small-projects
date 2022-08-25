50c3152e6771ae22a445cfea3d5b7a237a3f7681
King <pic.ner101@gmail.com>
2022-08-25 12:15:59 +0100

    finish 7

--- src/apps/app7/index.jsx
+++ src/apps/app7/index.jsx
@@ -2,37 +2,42 @@
 // How does it work?
 // when clicking next or prvious, thre items will be ready to move, one in the middle and two on the side which are hidden using overflow-hidden, the same time a the button clicked a set time-out function will will be trigged after the duration of animation, in that funciton the array will of displayed will be reformed and the animation will be cancled
 
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import listData from "./data";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 const App7 = () => {
+  const transitionDuration = 500; //75, 100, 150, 200,300, 500, 700, 1000
   const [moveForward, setMoveForward] = useState(false);
   const [moveBackward, setMoveBackward] = useState(false);
 
-  //   const [mainList, setMainList] = useState([
-  //     "one",
-  //     "two",
-  //     "three",
-  //     "four",
-  //     "five",
-  //     "six",
-  //   ]);
   const mainList = listData;
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [backUp, setBackup] = useState([]);
 
   // this method is used to handle incrase the index number when moving forward and backward to insure it does not go below 0 or above the array length
-  function updateSelectedIndex(movement) {
+  const updateSelectedIndex = (movement) => {
     console.log(movement);
+
     if (movement === "forward") {
-      if (selectedIndex >= mainList.length - 1) return setSelectedIndex(0);
-      return setSelectedIndex((prev) => prev + 1);
+      console.log("the seelectedIndex", selectedIndex);
+
+      console.log(
+        `(${selectedIndex} < ${mainList.length - 1})`,
+        selectedIndex < mainList.length - 1
+      );
+      if (selectedIndex < mainList.length - 1)
+        return setSelectedIndex((prev) => prev + 1);
+
+      return setSelectedIndex(0);
     }
     if (movement === "backward") {
-      if (selectedIndex <= 0) return setSelectedIndex(mainList.length - 1);
+      if (selectedIndex === 0) return setSelectedIndex(mainList.length - 1);
       return setSelectedIndex((prev) => prev - 1);
     }
-  }
+  };
 
-  const handleMoveForward = () => {
+  const handleMoveForward = useCallback(() => {
+    // console.count("counter");
     // when this is true the animation starts, and animation will be allowed
     setMoveForward(true);
     setTimeout(() => {
@@ -40,23 +45,26 @@ const App7 = () => {
       setMoveForward(false);
       //   this will increase the index, and carouselList is watching this value to reform the
       updateSelectedIndex("forward");
-    }, 100);
-  };
+    }, transitionDuration);
+  });
 
   const handleMoveBackward = () => {
     setMoveBackward(true);
     setTimeout(() => {
       setMoveBackward(false);
       updateSelectedIndex("backward");
-    }, 100);
+    }, transitionDuration);
   };
 
   // this will generete a new array based on the mainList  (from the original array) of three items, for display
   const carouselList = useMemo(() => {
+    if (selectedIndex < 0 || selectedIndex > listData.length - 1) return backUp;
     let listLength = mainList.length;
     let lastItem = mainList[listLength - 1];
+    // console.log("last item ", lastItem);
     let firstItem = mainList[0];
     let currentSlide = mainList[selectedIndex];
+    // console.log("last item ", mainList[selectedIndex - 1]);
     let previousSlide =
       selectedIndex === 0 ? lastItem : mainList[selectedIndex - 1];
     let nextSlide =
@@ -64,48 +72,120 @@ const App7 = () => {
         ? firstItem
         : mainList[selectedIndex + 1];
 
+    setBackup([previousSlide, currentSlide, nextSlide]);
+
     return [previousSlide, currentSlide, nextSlide];
   }, [selectedIndex]);
-  console.log(selectedIndex);
+  console.log("number", selectedIndex);
+
   console.log(carouselList);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleMoveForward();
+      // console.log("hola");
+    }, 2000);
+    return () => {
+      // console.log("mola");
+      return clearInterval(interval);
+    };
+  });
+
   return (
-    <div className="pn-app-container h-screen w-screen bg-slate-50 flex flex-col justify-center items-center ">
-      <h1 className=" capitalize text-gray-700 text-4xl h-fit ">
-        <span className=" text-red-400">/</span> <span>reviews</span>
+    <div className="pn-app-container h-screen w-screen bg-slate-50 flex flex-col  items-center pt-[5rem] px-[3%] ">
+      <h1 className=" capitalize text-gray-700 text-4xl h-fit mb-8 ">
+        <span className=" text-red-400 ">/</span> {"  "} <span>reviews</span>
       </h1>
-      <div className="pn-carousel h-28 w-28 bg-slate-300 relative overflow-hidden">
+      <div className="pn-carousel h-96 w-full max-w-3xl bg-slate-100 relative overflow-hidden">
         <article
-          className={`h-full w-full bg-yellow-200 top-0 left-[-100%]   absolute
+          className={`h-full w-full  top-0 left-[-100%]   absolute
+         flex flex-col justify-center items-center dur
+         ${moveForward && "duration-" + transitionDuration}   
+         ${moveBackward && "duration-" + transitionDuration}   
           ${moveForward && "-translate-x-[100%]"} 
           ${moveForward && "transition-all"}
           ${moveBackward && "translate-x-[100%]"} 
           ${moveBackward && "transition-all"}`}
         >
-          {carouselList[0].title}
+          <div
+            className={`pn-user-img  h-40 w-40 rounded-full  bg-cover bg-center flex bg-red-400 bg-clip-padding border-[5px] border-slate-200 shadow-xl mb-8 `}
+            style={{ backgroundImage: `url(${carouselList[0].image})` }}
+          ></div>
+          <h2 className=" text-lg font-medium uppercase text-red-900">
+            {carouselList[0].name}
+          </h2>
+          <h4 className=" text-gray-700 capitalize mb-8 ">
+            {carouselList[0].title}
+          </h4>
+          <p className=" text-slate-500  text-center text-sm ">
+            {carouselList[0].quote}
+          </p>
         </article>
         <article
-          className={`h-full w-full bg-yellow-200 absolute    
+          className={`h-full w-full  absolute 
+          flex flex-col justify-center items-center
+          ${moveForward && "duration-" + transitionDuration}   
+          ${moveBackward && "duration-" + transitionDuration}   
           ${moveForward && "-translate-x-[100%]"} 
           ${moveForward && "transition-all"} 
           ${moveBackward && "translate-x-[100%]"} 
-          ${moveBackward && "transition-all"}`}
+          ${moveBackward && "transition-all"}
+            `}
         >
-          {carouselList[1].title}
+          <div
+            className={`pn-user-img  h-40 w-40 rounded-full  bg-cover bg-center flex bg-red-400 bg-clip-padding border-[5px] border-slate-200 shadow-xl mb-8 `}
+            style={{ backgroundImage: `url(${carouselList[1].image})` }}
+          ></div>
+          <h2 className=" text-lg font-medium uppercase text-red-900">
+            {carouselList[1].name}
+          </h2>
+          <h4 className=" text-gray-700 capitalize mb-8 ">
+            {carouselList[1].title}
+          </h4>
+          <p className=" text-slate-500  text-center text-sm ">
+            {carouselList[1].quote}
+          </p>
         </article>
         <article
-          className={`h-full w-full bg-yellow-200 top-0 right-[-100%]  absolute 
+          className={`h-full w-full  top-0 right-[-100%]  absolute 
+
+          flex flex-col justify-center items-center
+          ${moveForward && "duration-" + transitionDuration}   
+          ${moveBackward && "duration-" + transitionDuration}    
           ${moveForward && "-translate-x-[100%]"} 
           ${moveForward && "transition-all"}
           ${moveBackward && "translate-x-[100%]"} 
           ${moveBackward && "transition-all"}
            `}
         >
-          {carouselList[2].title}
+          <div
+            className={`pn-user-img  h-40 w-40 rounded-full  bg-cover bg-center flex bg-red-400 bg-clip-padding border-[5px] border-slate-200 shadow-xl mb-8 `}
+            style={{ backgroundImage: `url(${carouselList[2].image})` }}
+          ></div>
+          <h2 className=" text-lg font-medium uppercase text-red-900">
+            {carouselList[2].name}
+          </h2>
+          <h4 className=" text-gray-700 capitalize mb-8 ">
+            {carouselList[2].title}
+          </h4>
+          <p className=" text-slate-500  text-center text-sm ">
+            {carouselList[2].quote}
+          </p>
         </article>
+        <button
+          onClick={handleMoveBackward}
+          className=" bg-slate-400 p-2 rounded absolute top-[50%] text-white  "
+        >
+          <FiChevronLeft />
+        </button>
+        <button
+          onClick={handleMoveForward}
+          className=" bg-slate-400 p-2 rounded absolute top-[50%] right-0 text-white  "
+        >
+          <FiChevronRight />
+        </button>
+        {/* <button onClick={() => Test()}>add</button> */}
       </div>
-      <button onClick={handleMoveBackward}>move backward</button>
-      <button onClick={handleMoveForward}>move forward</button>
     </div>
   );
 };
